db5be9f0e7736f477b41403148d4d7ddb0b90e8d
lehakurshev <91818719+lehakurshev@users.noreply.github.com>
2026-06-10 21:20:48 +0500

    Add textSummary import for K6 script

--- k6-tester/k6-script.js
+++ k6-tester/k6-script.js
@@ -1,6 +1,7 @@
 import http from 'k6/http'
 import { check, sleep } from 'k6'
 import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js'
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.4/index.js'
 import { SharedArray } from 'k6/data'
 
 const BASE_URL = 'http://backend:8080'
